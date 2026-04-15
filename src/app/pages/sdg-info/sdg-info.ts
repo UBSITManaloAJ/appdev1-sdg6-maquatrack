c9184672e6a4061b1374dd87280e32a2be125b85
UBSITManaloAJ <20243387@s.ubaguio.edu>
2026-04-15 12:03:34 +0800

    feat: initialize MaquaTrack PH Angular project for SDG 6

---
+++ src/app/pages/sdg-info/sdg-info.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sdg-info',
+  imports: [],
+  templateUrl: './sdg-info.html',
+  styleUrl: './sdg-info.css',
+})
+export class SdgInfo {}
