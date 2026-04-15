acde53200c8f182373cd1b2f60396425ca06fef1
UBSITManaloAJ <20243387@s.ubaguio.edu>
2026-04-15 12:16:05 +0800

    feat: add placeholder content for all pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { Tracker } from './pages/tracker/tracker';
+import { SdgInfo } from './pages/sdg-info/sdg-info';
+import { NotFound } from './pages/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '',          component: Home },
+  { path: 'home',      component: Home },
+  { path: 'dashboard', component: Dashboard },
+  { path: 'tracker',   component: Tracker },
+  { path: 'sdg-info',  component: SdgInfo },
+  { path: '**',        component: NotFound }
+];
